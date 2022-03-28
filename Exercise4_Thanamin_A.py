909ab7a6c0c406635dae30ebe9f9cd96e75e3388
Thanamin <32531724+Thanamin@users.noreply.github.com>
2022-03-28 12:35:28 +0700

    Exercise4_Thanamin_Akkharananwinit

---
+++ Exercise4_Thanamin_A.py
@@ -0,0 +1,10 @@
+score_FE = 65.0
+score_GB = 70.0
+score_ICS = 88.0
+score_CP = 85.0
+
+print("--- Your Score ---")
+print("Foundation English :",score_FE)
+print("General Business :",score_GB)
+print("Introduction to Computer Systems :",score_ICS)
+print("Computer Programming :",score_CP)
