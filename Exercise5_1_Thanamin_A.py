b03c2cc4dbfd25fcc28c1eee01f2f07e70b92eba
Thanamin <32531724+Thanamin@users.noreply.github.com>
2022-03-28 18:46:02 +0700

    Exercise5_1_Thanamin_A

---
+++ Exercise5_1_Thanamin_A.py
@@ -0,0 +1,7 @@
+Num1 = int(input("Num1 :"))
+Num2 = int(input("Num2 :"))
+print("Calculate +,-,*,/")
+print(Num1,"+",Num2,"=",Num1+Num2)
+print(Num1,"-",Num2,"=",Num1-Num2)
+print(Num1,"*",Num2,"=",Num1*Num2)
+print(Num1,"/",Num2,"=",int(Num1/Num2))
